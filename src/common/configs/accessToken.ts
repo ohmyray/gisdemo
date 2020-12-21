3ba8189f07c605fa4eb4487d5c835aeae949d16a
Ray <ohmyray@163.com>
2020-12-21 08:20:31 +0800

    添加地图声明调用

--- src/common/configs/accessToken.ts
+++ src/common/configs/accessToken.ts
@@ -1,7 +1,2 @@
-const token: string = 'pk.eyJ1Ijoib2hteXJheSIsImEiOiJja2FvMHlrNnUxd3FlMnNtcXQ2YzR6cDNhIn0.VOodHc8C2T6ldql4mnsSeg'
-
-export function accessToken() {
-    return function name(target:any) {
-        target = token;
-    }
-}
+export const accessToken: string =
+  'pk.eyJ1Ijoib2hteXJheSIsImEiOiJja2FvMHlrNnUxd3FlMnNtcXQ2YzR6cDNhIn0.VOodHc8C2T6ldql4mnsSeg'
--- src/map/map.ts
+++ src/map/map.ts
@@ -1,7 +1,25 @@
-declare var mapboxgl:any;
+import { MapboxOptions } from './interface/MapboxOptions'
+import { Map } from 'mapbox-gl'
+import { accessToken } from '@/common/configs'
 
-class MapBox {
-  constructor() {}
+class MapBox implements Map {
+  _pmap!: Map
+  _mapDefault!: MapboxOptions
+  constructor(options?: MapboxOptions) {
+    options && (options.accessToken = accessToken)
+    options && (this._pmap = new Map(options))
+  }
+
+  /**
+   * 
+   * @param options 
+   */
+  init(options?: MapboxOptions): MapBox {
+    if (!!options) this._mapDefault = options
+    if (!!!this._mapDefault) console.error(`参数为${options}!`)
+    this._mapDefault && (this._pmap = new Map(this._mapDefault))
+    return this
+  }
 }
 
-export { MapBox };
+export { MapBox }
